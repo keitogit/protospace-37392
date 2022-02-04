bba8bf0b2afc409a12758e14cbde85951f6cabd7
keitogit <kate_m@me.com>
2022-02-04 17:41:28 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,6 +2,7 @@ class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -58,5 +59,4 @@ class PrototypesController < ApplicationController
   def contributor_confirmation
     redirect_to root_path unless current_user == @prototype.user
   end
-
 end
