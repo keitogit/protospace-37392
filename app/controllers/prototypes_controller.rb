6025bf2db97e263fb711948df8b0c13280195532
keitogit <kate_m@me.com>
2022-02-07 10:47:06 +0900

    commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,6 @@ class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
-  
   def index
     @prototypes = Prototype.includes(:user)
   end
